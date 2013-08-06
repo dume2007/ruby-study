9a222241f5c656a46b8cd35268b397ca63caa50d
dume2007 <dume2007@gmail.com>
2013-08-07 01:01:31 +0800

    db

--- app/runtime/do.rb
+++ app/runtime/do.rb
@@ -3,6 +3,7 @@ exit if defined?(ENTRANCE).nil? || defined?(APP_NAME).nil?
 require 'mysql'
 require 'json'
 require './app/classes/db.rb'
+require './app/classes/app.rb'
 
 sql = "select * from category"
 Db.new.query(sql).each_hash do |item|
